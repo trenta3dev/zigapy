11b50fad87cc71d8119d7169d8de2ae0d32c53f9
Stefano Parmesan <stefanop@ahref.eu>
2012-10-02 22:02:57 +0200

    a bit of code cleanup

--- zigapy/__init__.py
+++ zigapy/__init__.py
@@ -14,21 +14,24 @@ class Channel(object):
     def __init__(self, ziga_obj, channel_name):
         self.ziga_obj = ziga_obj
         self.channel_name = channel_name
-        self.url = '{4}://{0}:{1}/{2}/{3}'.format(self.ziga_obj.host,
-                                                    self.ziga_obj.port,
-                                                    self.ziga_obj.app_key,
-                                                    self.channel_name,
-                                                    self.ziga_obj.protocol)
+        self.url = '{4}://{0}:{1}/{2}/{3}'.format(
+            self.ziga_obj.host, self.ziga_obj.port, self.ziga_obj.app_key,
+            self.channel_name, self.ziga_obj.protocol)
 
-    def trigger(self, event, data={}):
+    def trigger(self, event, data=None):
+        if data is None:
+            data = dict()
+
         to_post = {'event': event, 'data': data}
         headers = {'Content-type': 'application/json'}
-        response = requests.post(self.url,
-                                data=json.dumps(to_post),
-                                headers=headers)
-        #TODO: handle response codes here
+        response = requests.post(
+            self.url, data=json.dumps(to_post), headers=headers)
+
+        # TODO: handle response codes here
+        return response.status_code == 200
 
     def authenticate(self):
         headers = {'Content-type': 'application/json'}
         response = requests.post(self.url, data='{}', headers=headers)
-        return r.text
+        # TODO: do something smart here
+        return response.text
